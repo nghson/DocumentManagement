49edaade8b1c863dfa285ff02de09518340a6cfd
nghson <son.hngy@gmail.com>
2023-08-01 18:36:07 +0300

    TextFile domain class

--- src/main/java/docmanagement/TextFile.java
+++ src/main/java/docmanagement/TextFile.java
@@ -1,12 +1,37 @@
 package docmanagement;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
 import java.util.function.Predicate;
+import static docmanagement.Attributes.*;
+import static java.util.stream.Collectors.toList;
 
 class TextFile {
     private final Map<String, String> attributes;
     private final List<String> lines;
+
+    TextFile(final File file) throws IOException {
+        attributes = new HashMap<>();
+        attributes.put(PATH, file.getPath());
+        lines = Files.lines(file.toPath()).collect(toList());
+    }
+
+    Map<String, String> getAttributes() {
+        return attributes;
+    }
+
+    void addLineSuffix(final String prefix, final String attributeName) {
+        for (final String line: lines) {
+            if (line.startsWith(prefix)) {
+                attributes.put(attributeName, line.substring(prefix.length()));
+                break;
+            }
+        }
+    }
 
     int addLines(
             final int start,
