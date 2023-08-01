e352bb07666ee520f95289ba26c2e22214293bc7
nghson <son.hngy@gmail.com>
2023-08-01 17:50:22 +0300

    Attributes class

---
+++ src/main/java/docmanagement/Attributes.java
@@ -0,0 +1,8 @@
+package docmanagement;
+
+class Attributes {
+    public static final String PATH = "path";
+    public static final String TYPE = "type";
+    public static final String WIDTH = "width";
+    public static final String HEIGHT = "height";
+}
