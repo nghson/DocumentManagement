31a808ec7836712bba1b71c6d65644d56e0b23f0
nghson <son.hngy@gmail.com>
2023-08-01 17:47:03 +0300

    Document domain class

--- src/main/java/docmanagement/Document.java
+++ src/main/java/docmanagement/Document.java
@@ -1,5 +1,14 @@
 package docmanagement;
 
-public class Document {
+import java.util.Map;
 
+public class Document {
+    private final Map<String, String> attributes;
+    Document(final Map<String, String> attributes) {
+        this.attributes = attributes;
+    }
+
+    public String getAttribute(final String attributeName) {
+        return attributes.get(attributeName);
+    }
 }
